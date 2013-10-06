96d7b26687121cb6f7406a60c6292cba8b9406ca
sarathmvprakash <sarathmvprakash@gmail.com>
2013-10-06 14:37:51 +0530

    Creating basic android project GkQuiz

---
+++ GkQuiz/src/com/sarath/gkquiz/GkActivity.java
@@ -0,0 +1,14 @@
+package com.sarath.gkquiz;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class GkActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_gk);
+  }
+}
